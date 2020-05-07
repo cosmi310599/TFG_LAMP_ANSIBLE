8181a25e48eacd8cd18b9ac3f9c67e690f4a6f51
cosmi310599 <43874186+cosmi310599@users.noreply.github.com>
2020-05-07 22:38:48 +0200

    Delete conexion.php

--- LAMP/roles/mariadb/files/conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connection = new PDO('mysql:host=localhost;port=3306,dbname=saludo', 'root', 'Admin12345');
-$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-#Ponemos atributos para controlar errores y excepciones 
-?>
